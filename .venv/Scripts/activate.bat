324f82096c39b88f93db4a71bbd4798650c22f11
daoquimui <muidao156@gmail.com>
2024-10-16 07:34:17 +0700

    add gitignore

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\QuanLyMyPham\.venv
+set VIRTUAL_ENV=E:\DoAnNoSql\QuanLyMyPham\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
